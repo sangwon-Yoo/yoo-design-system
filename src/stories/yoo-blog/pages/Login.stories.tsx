0d1d8321b6dc27beb412fc94ac9f2edc7ea66424
YooSangwon <ysw1230@gmail.com>
2023-09-27 17:27:17 +0900

    ++login pate

--- src/stories/yoo-blog/pages/Login.stories.tsx
+++ src/stories/yoo-blog/pages/Login.stories.tsx
@@ -1,16 +1,16 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { Home } from "@/stories/yoo-blog/pages/Home";
+import { Login } from "@/stories/yoo-blog/pages/Login";
 
 const meta = {
     title: 'yoo-blog/pages',
-    component: Home,
+    component: Login,
     parameters: {
         // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
         layout: 'fullscreen',
     },
-} satisfies Meta<typeof Home>
+} satisfies Meta<typeof Login>
 export default meta;
 
-export const PageHome: StoryObj<typeof Home> = {
-    render: () => <Home />
+export const PageLogin: StoryObj<typeof Login> = {
+    render: () => <Login />
 };

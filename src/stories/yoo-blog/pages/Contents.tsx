4cd6d3263adb1f021b40ce458f68cbcbf41cb12a
YooSangwon <ysw1230@gmail.com>
2023-10-19 15:54:12 +0900

    ++contents

--- src/stories/yoo-blog/pages/Contents.tsx
+++ src/stories/yoo-blog/pages/Contents.tsx
@@ -259,7 +259,7 @@ export function Contents() {
 
 
                         <StyledWrapper $styled={{
-                            margin : '32px 0 0 0',
+                            margin : '32px 0 24px 0',
                             borderTop : '1px solid #e6e6e6',
                             borderBottom : '1px solid #e6e6e6'
                         }}>
@@ -314,6 +314,138 @@ export function Contents() {
                                 </StyledLayoutFlex>
                             </StyledContents>
                         </StyledWrapper>
+
+                        <StyledWrapper
+                            $styled={{ margin : '48px 0 36px 0' }}
+                        >
+                            <StyledContentsParagraph
+                                $styled={{
+                                    width : '680px',
+                                    height : 'auto',
+                                    fontSize : '1.2rem',
+                                    fontWeight : 'bold',
+                                    color : '#292929'
+                                }}
+                                $styledMobile={{
+                                    width : '100%',
+                                    height : 'auto',
+                                    fontSize : '1.1rem',
+                                    fontWeight : 'bold',
+                                    color : '#292929'
+                                }}
+                                as={'h3'}
+                            >
+                                부제목 부제목 부제목 부제목 부제목 부제목부제목 부제목 부제목부제목 부제목 부제목부제목 부제목 부제목부제목 부제목 부제목부제목 부제목 부제목
+                            </StyledContentsParagraph>
+                        </StyledWrapper>
+
+                        <StyledWrapper
+                            $styled={{ margin : '24px 0' }}
+                        >
+                            <StyledContentsParagraph
+                                $styled={{
+                                    width : '680px',
+                                    height : 'auto',
+                                    color : '#292929',
+                                    whiteSpace : 'pre-line'
+                                }}
+                                $styledMobile={{
+                                    width : '100%',
+                                    height : 'auto',
+                                    color : '#292929'
+                                }}
+                            >
+                                useEffect에 대해 자세히 알아보자. <b>생명주기를</b> 위해 무엇을 신경써야할까? <i style={{backgroundColor : '#66f1e1' }}>useEffect에 대해 자세히 알아보자.</i> 생명주기를 위해 무엇을 신경써야할까? useEffect에 대해 자세히 알아보자. 생명주기를 위해 무엇을 신경써야할까? useEffect에 대해 자세히 알아보자. 생명주기를 위해 무엇을 신경써야할까? useEffect 에 대해 자세히 알아보자. 생명주기를 위해 무엇을 신경써야할까? useEffect에 대해 자세히 알아보자. 생명주기를 위해 무엇을 신경써야할까?
+                            </StyledContentsParagraph>
+                        </StyledWrapper>
+
+                        <StyledWrapper
+                            $styled={{ margin : '24px 0' }}
+                        >
+                            <StyledContentsParagraph
+                                $styled={{
+                                    width : '680px',
+                                    height : 'auto',
+                                    color : '#292929',
+                                    whiteSpace : 'pre-line',
+                                    textAlign : 'center'
+                                }}
+                                $styledMobile={{
+                                    width : '100%',
+                                    height : 'auto',
+                                    color : '#292929'
+                                }}
+                            >
+                                useEffect에 대해 자세히 알아보자.
+                            </StyledContentsParagraph>
+                        </StyledWrapper>
+
+                        <StyledWrapper
+                            $styled={{ margin : '24px 0' }}
+                        >
+                            <StyledContentsParagraph
+                                $styled={{
+                                    width : '680px',
+                                    height : 'auto',
+                                    color : '#292929',
+                                    whiteSpace : 'pre-line'
+                                }}
+                                $styledMobile={{
+                                    width : '100%',
+                                    height : 'auto',
+                                    color : '#292929'
+                                }}
+                            >
+                                useEffect에 대해 자세히 알아보자. <b>생명주기를</b> 위해 무엇을 신경써야할까? <i style={{ backgroundColor : '#66f1e1' }}>useEffect에 대해 자세히 알아보자.</i> 생명주기를 위해 무엇을 신경써야할까? useEffect에 대해 자세히 알아보자. 생명주기를 위해 무엇을 신경써야할까? useEffect에 대해 자세히 알아보자. 생명주기를 위해 무엇을 신경써야할까? useEffect 에 대해 자세히 알아보자. 생명주기를 위해 무엇을 신경써야할까? useEffect에 대해 자세히 알아보자. 생명주기를 위해 무엇을 신경써야할까?
+                            </StyledContentsParagraph>
+                        </StyledWrapper>
+
+                        <StyledWrapper
+                            $styled={{ margin : '48px 0 36px 0' }}
+                        >
+                            <StyledContentsParagraph
+                                $styled={{
+                                    width : '680px',
+                                    height : 'auto',
+                                    fontSize : '1.2rem',
+                                    fontWeight : 'bold',
+                                    color : '#292929'
+                                }}
+                                $styledMobile={{
+                                    width : '100%',
+                                    height : 'auto',
+                                    fontSize : '1.1rem',
+                                    fontWeight : 'bold',
+                                    color : '#292929'
+                                }}
+                                as={'h3'}
+                            >
+                                상태관리를 위한 툴
+                            </StyledContentsParagraph>
+                        </StyledWrapper>
+
+                        <StyledWrapper
+                            $styled={{ margin : '24px 0' }}
+                        >
+                            <StyledContentsParagraph
+                                $styled={{
+                                    width : '680px',
+                                    height : 'auto',
+                                    color : '#292929',
+                                    whiteSpace : 'pre-line'
+                                }}
+                                $styledMobile={{
+                                    width : '100%',
+                                    height : 'auto',
+                                    color : '#292929'
+                                }}
+                            >
+                                {`상태관 In San Francisco, there is a fresh crime wave that no DA can stop.\n Emboldened criminals are rushing around the streets wearing backpacks with suits, tan shoes with dark worsted slacks, and Patagonia fleece vests with chinos, dress shirts, and Allbird sneakers. Let’s take a look at some of the city’s most common fashion offenses today, as well as potential solutions.`}
+                            </StyledContentsParagraph>
+                        </StyledWrapper>
+
+
+
                     </StyledWrapper>
                 </StyledLayoutFlexItem>
             </StyledLayoutFlex>
